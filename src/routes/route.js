180ad41e4cb75d02bb28f654bc00444541eaa55f
Saurabh Tripathi <saurabhtripathitripathi1997@gmail.com>
2022-04-09 18:25:51 +0530

    Api

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,68 +2,42 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    // let a = { msg: "My first ever API response in JSON !!"} 
-
-
-    res.send( { msg: "My first ever API response in JSON !!"} )
-});
 
 
 
-router.get('/test-api1', function (req, res) {
+let players=[]
 
-    res.send( "hi FunctionUp " )
-});
+router.post('/players', function(req, res){
 
+   
 
-router.get('/test-api2', function (req, res) {
+    let player= req.body
 
-    res.send( { msg: "Hi FUnctionUp..again !"} )
-});
+    let playerName = players.name
 
+    for(let i=0;i<players.length;i++)
 
-router.get('/test-api3', function (req, res) {
+    {
 
-    res.send( { msg: "Hi FUnctionUp..again..this is another similar api !"} )
-});
+        if(players[i].name == playerName)
 
+        {
 
-router.get('/test-api4', function (req, res) {
+           return  req.send('players not exits')
 
-    res.send( { msg: "Hi FUnctionUp..again..this is another similar api ..not I am getting bored!"} )
-});
+        }
 
+    }
 
-router.get('/test-api5', function (req, res) {
+    players.push(player)
 
-    res.send( { msg: "Hi FUnctionUp" , name:"FunctionUp", age: "100"} )
-});
+    console.log('here is the player from requst',players)
 
+    res.send(players)
 
-
-router.get('/test-api6', function (req, res) {
-
-    res.send( {   data: [12, 24, 36, 48, 60]  }   )
-});
-
-router.post('/test-post1', function (req, res) {
-
-    res.send( {  msg: "hi guys"  }   )
-});
+})
 
 
-// to send data in  post request-> prefer sending in BODY -> click body-raw-json
-router.post('/test-post2', function (req, res) {
-    let data= req.body
-    console.log(data)
-    res.send( {  msg: "hi guys..my 2nd post req"  }   )
-});
-
-
-const randomController= require("../controllers/randomController.js")
-//write a post request to accept an element in post request body and add it to the given array and return the new array
-router.post('/test-post3', randomController.addToArray ); //HANDLER/CONTROLLER
 
 
 
