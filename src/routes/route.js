bc1cea2e9ee6b3ac45898a851a74e2f9e9bb8dc6
Saurabh Tripathi <saurabhtripathitripathi1997@gmail.com>
2022-04-19 17:28:41 +0530

    papulate2

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const authorController = require("../controllers/authorController")
-const bookController = require("../controllers/bookController")
-const publisherController = require("../controllers/publisherController")
+const batchController = require("../controllers/batchController")
+const developerController = require("../controllers/developerController")
+
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createAuthor", authorController.createAuthor  )
-router.post("/createPublisher", publisherController.createPublisher)
-router.post("/createBook", bookController.createBook  )
-router.get("/getBooks", bookController.getBooks)
-router.put('/books', bookController.updateBooks)
+router.post("/c ", batchController.createBatch  )
+router.post("/createdeveloper", developerController.createdeveloper)
+// router.post("/createBook", bookController.createBook  )
+ router.get("/scholarship-developers", developerController.scholardevelopers)
+// router.put('/books', bookController.updateBooks)
 
 module.exports = router;
-
-
-
 module.exports = router;
