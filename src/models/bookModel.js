0640959c954b68634337c19c1a7229970a65172a
Saurabh Tripathi <saurabhtripathitripathi1997@gmail.com>
2022-04-21 08:28:31 +0530

    hii

---
+++ src/models/bookModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema( {
+    bookName: String, 
+    authorName: String, 
+    tags: [String],
+    
+    isPublished: Boolean,
+    prices: {
+        indianPrice: String,
+        europePrice: String,
+    },
+    sales: {type: Number, default: 10},
+    
+    
+    summary :  mongoose.Schema.Types.Mixed,
+    isDeleted: Boolean 
+
+}, { timestamps: true });
+
+
+module.exports = mongoose.model('Book', bookSchema) //users
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const controller = require('../controllers/controller')
+const mw = require("../middleware/middleware")
 
-const batchController = require("../controllers/batchController")
-const developerController = require("../controllers/developerController")
+
+//1 creating User
+router.post('/createUser', mw.headerCheck, controller.createUser)
+
+//2 creating Product
+router.post('/createProduct',controller.createProduct)
+
+//3 creating order
+router.post('/createOrder',mw.headerCheck, controller.createOrder)
 
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
-
-router.post("/c ", batchController.createBatch  )
-router.post("/createdeveloper", developerController.createdeveloper)
-// router.post("/createBook", bookController.createBook  )
- router.get("/scholarship-developers", developerController.scholardevelopers)
-// router.put('/books', bookController.updateBooks)
-
-module.exports = router;
 module.exports = router;
